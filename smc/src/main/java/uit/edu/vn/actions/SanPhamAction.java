7d44d7c04729b00d8b2a0b6f43476e4e3d44f798
huyvu96 <huyvu0505@gmail.com>
2018-05-07 14:20:06 +0700

    Update 7/5

--- smc/src/main/java/uit/edu/vn/actions/SanPhamAction.java
+++ smc/src/main/java/uit/edu/vn/actions/SanPhamAction.java
@@ -2,26 +2,24 @@ package uit.edu.vn.actions;
 import java.util.ArrayList;
 import java.util.List;
 import com.opensymphony.xwork2.ActionSupport;
-import uit.edu.vn.models.CuaHang;
-import uit.edu.vn.utils.DataCuaHang;
+import uit.edu.vn.models.SanPham;
+import uit.edu.vn.utils.DataSanPham;
 
 public class SanPhamAction  extends ActionSupport{
 	private static final long serialVersionUID = 1L;
-	DataCuaHang dbCuaHang = new DataCuaHang();
-	private List<CuaHang> lstCuaHang = new ArrayList<CuaHang>();
-
-    public List<CuaHang> getLstCuaHang() {
-		return lstCuaHang;
+	DataSanPham dbSanPham= new DataSanPham();
+	private List<SanPham> lstSanPham = new ArrayList<SanPham>();
+   
+	public List<SanPham> getLstSanPham() {
+		return lstSanPham;
 	}
-
-	public void setLstCuaHang(List<CuaHang> lstCuaHang) {
-		this.lstCuaHang = lstCuaHang;
+	public void setLstSanPham(List<SanPham> lstSanPham) {
+		this.lstSanPham = lstSanPham;
 	}
-	
 	@Override
     public String execute() throws Exception {
-		lstCuaHang = dbCuaHang.getLstCuaHangFromDb();
-		System.out.print(lstCuaHang.size());
+		this.lstSanPham = dbSanPham.getDsSanPhamFromDb();
+		System.out.print(lstSanPham.get(1).getTenSanPham());
         return SUCCESS;
     }
 }
