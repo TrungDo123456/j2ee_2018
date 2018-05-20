4f4f6a9f93c46d740d05cdbd0583aa3d19db6784
Hàng Tuấn Thiên <hangtuanthiendl@gmail.com>
2018-05-20 20:26:43 +0700

    Thêm hàm dùng chung: + Hàm test chuỗi có "" hoặc null ko

---
+++ smc/src/main/java/uit/edu/vn/utils/HamDungChung.java
@@ -0,0 +1,8 @@
+package uit.edu.vn.utils;
+
+public final class HamDungChung {
+	public static boolean isString(String string) {
+		if(string == "" || string == null) return false;
+		return true;
+	}
+}
